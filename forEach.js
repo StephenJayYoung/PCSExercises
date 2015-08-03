3dc66273dcc2dc856e611302eae7798f01b901e8
Stephen Young <stephenjayyoung@gmail.com>
2015-08-03 14:48:03 -0700

    wrapping up for now

---
+++ forEach.js
@@ -0,0 +1,28 @@
+function logArrayElements(element, index, array) {
+  console.log('a[' + index + '] = ' + element);
+}
+
+// Note elision, there is no member at 2 so it isn't visited
+[2, 5, , 9,10].forEach(logArrayElements);
+// logs:
+// a[0] = 2
+// a[1] = 5
+// a[3] = 9
+
+////////////////////////////////
+
+function letsEatCake (a, b, c){
+return (this.a +" and " + this.b + " and " + this.c);
+}
+var o = {a:"german chocolate", b:"carrot cake", c:"confetti"}
+
+// letsEatCake.apply(o);
+
+console.log(letsEatCake.apply(o));
+
+//this will log all of the elements (due to forEach) at indexes 
+//0, 1, 2 of the array (which are 1,2,3 in this array)
+[1,2,3,].forEach(logArrayElements);
+// console.log("break");
+// [this.a, this.b, this.c].forEach(logArrayElements);
+// [o].forEach(logArrayElements);

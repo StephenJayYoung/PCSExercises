11d9fa88eaf17baadb88c12e848329740b12f334
Stephen Young <stephenjayyoung@gmail.com>
2015-08-03 18:51:40 -0700

    more additions to class exercises

--- Aug3.js
+++ Aug3.js
@@ -11,7 +11,24 @@ function paint(obj) {
 }
 
 //call paint as a method
-var obj = {
-	obj.paint = paint;
-}
+var obj = {}
+obj.paint = paint;
 obj.paint();
+
+//use paint as a callback with forEach on some array of objects
+
+
+//use forEach
+var objs = [{name:'fence'},{name:'car'},{name:'bicycle'}];
+
+objs.forEach(paint);
+console.log(objs.forEach(paint));
+
+//replace the reference to paint as a function
+//definition in that same spot
+
+objs.forEach(function(blah){
+	blah.color='blue'
+})
+
+console.log(objs);

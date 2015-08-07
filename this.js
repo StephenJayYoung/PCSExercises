6f1a17e64030084bff7e5474b3edd220e082d26f
Stephen Young <stephenjayyoung@gmail.com>
2015-08-06 17:05:43 -0700

    added some code to this.js

--- this.js
+++ this.js
@@ -38,6 +38,38 @@ console.log(addAndRoundDown.apply(o));
 
 ///////////////////////////////////////////
 
+var person = {
+firstName:"Penelope",
+lastName:"Barrymore",
+nickName: "DaHotness",
+// Since the "this" keyword is used inside the showFullName method below, and the showFullName method is defined on the person object,​
+// "this" will have the value of the person object because the person object will invoke showFullName ()​
+showFullName:function () {
+console.log (this.firstName + " " + this.lastName + " " + "aka " + this.nickName);
+}
+}
+console.log(person.firstName);
+person.showFullName (); // Penelope Barrymore
 
+/////////////////////////////////////////////
+var firstName = "Peter",
+lastName = "Ally";
 
-
+function showFullName () {
+// "this" inside this function will have the value of the window object​
+// because the showFullName () function is defined in the global scope, just like the firstName and lastName​
+console.log (this.firstName + " " + this.lastName);
+}
+var person = {
+firstName   :"Penelope",
+lastName    :"Barrymore",
+showFullName:function () {
+// "this" on the line below refers to the person object, because the showFullName function will be invoked by person object.​
+console.log (this.firstName + " " + this.lastName);
+}
+}
+showFullName (); // Peter Ally​
+// window is the object that all global variables and functions are defined on, hence:​
+// window.showFullName (); // Peter Ally​
+// "this" inside the showFullName () method that is defined inside the person object still refers to the person object, hence:​
+// person.showFullName (); // Penelope Barrymore

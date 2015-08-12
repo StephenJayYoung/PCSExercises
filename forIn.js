54ae84ea94693354335dae8e2fa6e6742e21c90c
Stephen Young <stephenjayyoung@gmail.com>
2015-08-12 15:10:22 -0700

    played with forin statements a little more, added some comments

--- forIn.js
+++ forIn.js
@@ -1,26 +1,47 @@
-var obj = {a:1, b:2, c:3, d:"apple", e: "elephant", f: true};
+// The for..in statement iterates over the enumerable properties of an object,
+//  in arbitrary order. For each distinct property, statements can be executed.
 
-for (var prop in obj) {
-  console.log("o." + prop + " = " + obj[prop]);
-}
-console.log(prop);
-console.log(obj[prop]);
-console.log(obj.a);
-console.log(obj.d);
+//So, basically, we can iterate over all of the objects in an array, and then 
+//return the properties of those objects (and not the keys);
+
+var stuff = {a:1, b:2, c:3, d:"apple", e: "elephant", f: true};
+
+// for (var prop in stuff) {
+//   console.log(stuff[prop]);
+// }
+
+console.log(stuff);
+// console.log(prop);
+// console.log(obj[prop]);
+// console.log(obj.a);
+// console.log(obj.d);
 //////////////////////////////////////
 
-var triangle = {a:1, b:2, c:3};
+// var coloredFurniture = {
+// couch: "red",
+// chair: "blue",
+// bed: "white"
+// };
 
-function ColoredTriangle() {
-  this.color = "red";
-}
-//.prototype means
-ColoredTriangle.prototype = triangle;
+// console.log(coloredFurniture);
 
-var obj = new ColoredTriangle();
+// for (var prop in coloredFurniture) {
+// 	// console.log(prop);
+// 	console.log(coloredFurniture);
+// }
 
-for (var prop in obj) {
-  if( obj.hasOwnProperty( prop ) ) {
-    console.log("o." + prop + " = " + obj[prop]);
-  } 
-}
+// var triangle = {a:1, b:2, c:3};
+
+// function ColoredTriangle() {
+//   this.color = "red";
+// }
+// //.prototype means
+// ColoredTriangle.prototype = triangle;
+
+// var obj = new ColoredTriangle();
+
+// for (var prop in obj) {
+//   if( obj.hasOwnProperty( prop ) ) {
+//     console.log("o." + prop + " = " + obj[prop]);
+//   } 
+// }

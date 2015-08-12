2e6bada9f30126dc4aa4df40b5506e891d423197
Stephen Young <stephenjayyoung@gmail.com>
2015-08-11 21:43:56 -0700

    started working on some code for a module pattern, but having some trouble getting it to run

--- forIn.js
+++ forIn.js
@@ -7,3 +7,20 @@ console.log(prop);
 console.log(obj[prop]);
 console.log(obj.a);
 console.log(obj.d);
+//////////////////////////////////////
+
+var triangle = {a:1, b:2, c:3};
+
+function ColoredTriangle() {
+  this.color = "red";
+}
+//.prototype means
+ColoredTriangle.prototype = triangle;
+
+var obj = new ColoredTriangle();
+
+for (var prop in obj) {
+  if( obj.hasOwnProperty( prop ) ) {
+    console.log("o." + prop + " = " + obj[prop]);
+  } 
+}
---
+++ modulePattern.js
@@ -0,0 +1,22 @@
+var gasolineLevel = 10;
+
+function useGas(amt){
+	if(gasolineLevel - amt < 0) {
+		console.log("out of gas :[");
+	} else {
+		gasolineLevel -= amt;
+	}
+};
+
+return {
+	radioStation: "104.5",
+
+	changeStation: function(station){
+		this.radioStation = station;
+	},
+	go: function(speed){ useGas(speed); }
+	};
+};
+
+var ferrari = Car()
+ferrari.go(10);

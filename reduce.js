f725ef53ccaa7de968b9a06a0d729860a73f4ada
Stephen Young <stephenjayyoung@gmail.com>
2015-08-14 16:12:05 -0700

    added a .reduce function example

--- reduce.js
+++ reduce.js
@@ -1,8 +1,9 @@
-function addOne(total,item) { return total +1;}
+function addOne(total,item) { return total + item.length;}
 
 // fish.reduce(addOne);
 
 var listOfWords = ["yo", "what", "words"];
 
-listOfWords.reduce(addOne);
+listOfWords.reduce(addOne,0);
+
 

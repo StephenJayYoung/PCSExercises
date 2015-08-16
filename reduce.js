63f385200cb0305b9ede579e5c08a97da99cf88a
Stephen Young <stephenjayyoung@gmail.com>
2015-08-16 09:28:51 -0700

    worked on reduce example

--- reduce.js
+++ reduce.js
@@ -1,9 +1,18 @@
-function addOne(total,item) { return total + item.length;}
+function addOne(total,item) { 
+	return total + item.length
+	;}
 
 // fish.reduce(addOne);
 
 var listOfWords = ["yo", "what", "words"];
 
 listOfWords.reduce(addOne,0); 
+console.log(listOfWords.reduce(addOne,0));
 
+function subtractOne(total, thing) {
+	return total - thing.length
+};
 
+var allTheThings = ["one","two","three","four","five"];
+listOfWords.reduce(subtractOne,0);
+console.log(listOfWords.reduce(subtractOne,0));

a5db14afc73aa2e61d1a3874518ab05ec6d6fd11
Stephen Young <stephenjayyoung@gmail.com>
2015-08-17 19:53:27 -0700

    added anobject.js, not sure what I want to do with it yet

---
+++ anobject.js
@@ -0,0 +1,10 @@
+var sanJuans = {
+	activities: ["fishing", "biking", "running", "eatingAllTheFood","hiking"],
+	people: ["Isa", "Stephen"],
+	islands: ["Orcas", "Friday Harbor"]
+	function doTheThings(){
+
+	};
+
+}
+

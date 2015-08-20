d5f3625b1487e5d47e88e87ca52913cfcdf04721
Stephen Young <stephenjayyoung@gmail.com>
2015-08-20 15:49:29 -0700

    added another exercise

--- closures.js
+++ closures.js
@@ -9,3 +9,22 @@ var storeOassword = function(passwd) {
 	};
 return verifyPassword
 }
+
+///////////////////////////////////////////////////////////
+///Iffy: an immedietly invoked function
+///
+var factory = (function) {
+	var _id = 0;
+	return function () {
+		var thingID = _id++
+			return {
+				id: function() {
+					return thingID;
+				},
+				color: function() {
+					return (this.id() % 2) ? 'red' : 'blue';
+				}
+			};
+	}
+})();
+

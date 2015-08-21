37b96d8f98b7d2fcf7834836e7a3e2a2e63a4956
Stephen Young <stephenjayyoung@gmail.com>
2015-08-21 13:39:52 -0700

    Added a constructor exercise

--- closures.js
+++ closures.js
@@ -28,3 +28,45 @@ var factory = (function) {
 	}
 })();
 
+///////////////////////////////////////
+var array = ["hello", "world"];
+var callbacks = [];
+
+//create a loop and iterate
+for (var i = 0; i < array.length; i++) {
+  //push the function into the array callbacks
+  callbacks.push(function() {
+  	//log the elements of array
+    console.log(array[i]);
+  });
+}
+
+//push the function into the array foreach. 
+callbacks.forEach(function(c) {
+ c();
+});
+
+//the first thing should be a function that console.logs "hello", and a second
+//thing that console.logs "world"
+//
+////////////////////////////////////////////////////////////////
+var makeUser = (function() {// begin IIFE...
+	
+	var sharedLog = [];  //private; accessible only from functions defined within IIFE
+	// The factory itself:
+	function makeUser(name,passwd) {
+		// Return a user object with three methods:
+		// getName()
+		// validate(str)
+		// record(message) (Part b)
+	}
+	// Part b) only:
+	// Factory method (defined within IIFE, so can access sharedLog):
+	makeUser.getLog = function(user) {
+	}
+	return makeUser;
+})();
+if (typeof module != 'undefined')
+	module.exports = makeUser;
+
+
---
+++ prototypes.js
@@ -0,0 +1,31 @@
+
+//How are these different?
+function A() {};
+//set default values for instances of A:
+A.prototype = {num:0, str:'default'};
+var objA = new A();
+console.log(objA);
+
+
+function B() {};
+// set default values for instances of B:
+B.prototype.num = 0;
+B.prototype.str = 'default';
+var objB = new B();
+console.log(B);
+
+//ObjA is missing a value that makes it a constructor
+//To fix it, do this:
+function A() {};
+//set default values for instances of A:
+A.prototype = {num:0, str:'default', constructor:A};
+var objA = new A();
+console.log(objA);
+
+
+function B() {};
+// set default values for instances of B:
+B.prototype.num = 0;
+B.prototype.str = 'default';
+var objB = new B();
+console.log(objB);

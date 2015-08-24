cd0389b2c90fc250771d9016a234f30452c1f1ee
Stephen Young <stephenjayyoung@gmail.com>
2015-08-24 12:56:51 -0700

    added an inheritance2 file for some examples

---
+++ inheritance2.js
@@ -0,0 +1,17 @@
+//Inheritance is a programming technique that lets you share the same code 
+//between similar types
+
+function Media(title, duration) {
+	this.title = title;
+	this.duration = duration;
+	this.isPlaying = false;
+}
+
+Media.prototype.play = function() {
+	this.isPlaying = true;
+};
+
+Media.prototype.stop = function() {
+	this.isPlaying = false;
+}
+

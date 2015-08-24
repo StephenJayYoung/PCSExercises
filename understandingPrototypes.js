ac6373892ba2db21f04e653e7c3f21b4a6b46342
Stephen Young <stephenjayyoung@gmail.com>
2015-08-24 12:19:38 -0700

    revisions to understandingprototypes.js

--- understandingPrototypes.js
+++ understandingPrototypes.js
@@ -9,18 +9,20 @@ function Dice(sides) {
 
 var dice = new Dice(6);
 var dice10 = new Dice(10);
-
+console.log(dice.roll === dice10.roll)
 //The above code is the same as the bottom code, except with the bottom code we use
 //prototype
 
-function diceRoll() {
-	var randonNumber = MAth.floor(Math.random() * this.sides) + 1;
-	return randonNumber;
-}
 function Dice(sides) {
 	this.sides = sides;
-	this.roll = diceRoll;
 }
+
+Dice.prototype.roll = function () {
+		var randonNumber = Math.floor(Math.random() * this.sides) + 1;
+		return randonNumber;
+}
+
 
 var dice = new Dice(6);
 var dice10 = new Dice(10);
+console.log(dice.roll === dice10.roll)

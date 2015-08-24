67a83f97c070666112f5779883cb5d7926e59f28
Stephen Young <stephenjayyoung@gmail.com>
2015-08-24 13:08:05 -0700

    updating exercise

--- inheritance2.js
+++ inheritance2.js
@@ -1,17 +1,25 @@
 //Inheritance is a programming technique that lets you share the same code 
 //between similar types
 
+function Media(title, artist, duration) {
+	Media.call(this, title, duration);
+	this.artist = artist;
+}
+
+Song.prototype = Object.create(Media.prototype);
+
+
+
 function Media(title, duration) {
 	this.title = title;
 	this.duration = duration;
 	this.isPlaying = false;
 }
 
-Media.prototype.play = function() {
+Media.prototype.play = function(){
 	this.isPlaying = true;
-};
+}
 
 Media.prototype.stop = function() {
 	this.isPlaying = false;
 }
-

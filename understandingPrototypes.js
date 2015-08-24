2a26b59792d6a73881d4f28a98eb94b76c8e9be0
Stephen Young <stephenjayyoung@gmail.com>
2015-08-24 12:46:21 -0700

    added some new exerciese:

--- understandingPrototypes.js
+++ understandingPrototypes.js
@@ -22,7 +22,24 @@ Dice.prototype.roll = function () {
 		return randonNumber;
 }
 
+console.log(Dice.roll(5));
 
 var dice = new Dice(6);
 var dice10 = new Dice(10);
 console.log(dice.roll === dice10.roll)
+
+//Using the above and creating a new example
+function Rounder(round) {
+	this.round = round;
+}
+
+Rounder.prototype.doIt = function() {
+	var rounding = Math.ceil(this.round);
+	return rounding;
+}
+
+var round = new Rounder(1.75);
+var round1 = new Rounder(10.1);
+console.log(round.doIt);
+console.log(round1.doIt);
+

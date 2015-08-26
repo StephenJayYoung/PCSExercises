0992a03b78981188fee934648c9b7946abdbea25
Stephen Young <stephenjayyoung@gmail.com>
2015-08-26 10:20:31 -0700

    added an assertion example

---
+++ prototypes1.js
@@ -0,0 +1,9 @@
+//inheritance let's you share the same code between similar types
+
+//when there are objects with similar properties and behaviors, 
+//making constructor functions for each of them is a lot fo work,
+//so we use the prototype chain
+
+//when you access a property or method on an object, the javascript interpreter checks
+//if it's directly on the instance. If it's not, it will check the prototype. If it's 
+//not there, it will check the next prototype up the chain

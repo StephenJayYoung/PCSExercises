850c0befe4edd75d956160548b1672b29684b0bb
Stephen Young <stephenjayyoung@gmail.com>
2015-08-26 09:21:37 -0700

    added some comments to constructor example for further understanding

--- constructors.js
+++ constructors.js
@@ -1,6 +1,16 @@
+//Constructor functions describe how an object should be created
+//Good for creating multiple objects of the same type
+//each object created is known as an inctance of that object
+//
+
+
+//this is a constructor function
 function Contact(name, email) {
-	this.name = name; //"this" means a new instance of name
+	this.name = name; //"this" means a new instance of name.
 	this.email = email;
+	return this; //this is not necessary, but could be done
 }
+//this is the instance of the object (it's inherited from the object)
+//"new" turns a regular function call into a constructor function
  var contact = new Contact("Steve", "StephenJayYoung@gmail.com");
  ////////////////////////////////////////////////

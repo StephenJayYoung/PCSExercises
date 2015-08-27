424b953b0ad5c0094bd02111523809d6da04bb71
Stephen Young <stephenjayyoung@gmail.com>
2015-08-27 16:16:49 -0700

    added a toString example

---
+++ toString.js
@@ -0,0 +1,10 @@
+function Dog(name, breed, color, sex) {
+  this.name = name;
+  this.breed = breed;
+  this.color = color;
+  this.sex = sex;
+}
+
+theDog = new Dog('Gabby', 'Lab', 'chocolate', 'female');
+
+theDog.toString(); // returns [object Object]
